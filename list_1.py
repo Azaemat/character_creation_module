e99d72836889e2b3f29fd18ec2fe7a0d2f7e4d80
Азамат Загахов <Azaemat@yandex.ru>
2023-01-08 16:57:52 +0300

    add working version without classes

--- list_1.py
+++ list_1.py
@@ -1,23 +1,68 @@
-from math import sqrt
+class Human:
+    def __init__(self, name):
+        self.name = name
 
-message = ('Добро пожаловать в самую лучшую программу '
-           'для вычисления квадратного корня из заданного числа')
-print(message)
-
-
-def calculate_square_root(number):
-    """Вычисляет квадратный корень."""
-    return sqrt(number)
-
-
-def calc(your_number):
-    """Ввод результата."""
-    if your_number <= 0:
-        return
-    root = calculate_square_root(your_number)
-    print('Мы вычислили квадратный корень из введённого вами числа. '
-          f'Это будет: {root}')
+    # ответ по умолчанию для всех одинаковый, можно 
+    # доверить его родительскому классу
+    def answer_question(self, question):
+        
+        print(f'{self.name}, {question}\nОчень интересный вопрос! Не знаю.')
 
 
-print(message)
-calc(25.5)
+class Student(Human):
+    #  метод ask_question() принимает параметр someone:
+    #  это объект, экземпляр класса Curator, Mentor или CodeReviewer,
+    #  которому Student задаёт вопрос; 
+    #  параметр question — это просто строка
+    #  имя объекта и текст вопроса задаются при вызове метода ask_question
+    def ask_question(self, someone, question):
+        self.someone = someone
+        self.question = question
+        
+        result = someone.answer_question(question)
+        if result != None:
+            print(result)
+            
+        print()  # этот print выводит разделительную пустую строку	
+
+
+class Curator(Human):
+    def answer_question(self, question):
+        if question == 'мне грустненько, что делать?':
+            return (f'{self.name}, {question}\nДержись, всё получится. Хочешь видео с котиками?')
+        return super().answer_question(question)
+        # здесь нужно проверить, пришёл куратору знакомый вопрос или нет
+        # если да - ответить на него
+        # если нет - вызвать метод answer_question() у родительского класса
+
+# объявите и реализуйте классы CodeReviewer и Mentor
+class CodeReviewer(Human):
+    def answer_question(self, question):
+        if question == ('что не так с моим проектом?'):
+            return (f'{self.name}, {question}\nО, вопрос про проект, это я люблю.')
+        else:
+          return super().answer_question(question)    
+    
+class Mentor(Human):
+    def answer_question(self, question):
+        if question == 'как устроиться работать питонистом?':
+            return (f'{self.name}, {question}\nСейчас расскажу.')
+        elif question == 'мне грустненько, что делать?':
+                return (f'{self.name}, {question}\nОтдохни и возвращайся с вопросами по теории.')  
+                   
+        return super().answer_question(question)
+            
+
+# следующий код менять не нужно, он работает, мы проверяли
+student1 = Student('Тимофей')
+curator = Curator('Марина')
+mentor = Mentor('Ира')
+reviewer = CodeReviewer('Евгений')
+friend = Human('Виталя')
+ 
+student1.ask_question(curator, 'мне грустненько, что делать?')
+student1.ask_question(mentor, 'мне грустненько, что делать?')
+student1.ask_question(reviewer, 'когда каникулы?')
+student1.ask_question(reviewer, 'что не так с моим проектом?')
+student1.ask_question(friend, 'как устроиться на работу питонистом?')
+student1.ask_question(mentor, 'как устроиться работать питонистом?')
--- list_2.py
+++ list_2.py
@@ -1,28 +1,123 @@
-"""Документация модуля. Описывает работу классов и функций.
-Размещается в верхней части файла (начиная с первой строки).
-"""
-# Импортируйте модуль inspect.
-
-import inspect
+from random import randint
 
 
-def tricky_func(self):
-    """Описывает работу функции tricky_func."""
+from graphic_arts.start_game_banner import run_screensaver
+
+
+class Character:
+    def __init__(self,name):
+        self.name = name
+        
+
+class Warrior(Character):
     ...
 
 
-class Test:
-    """Класс Test используется для демонстрации docstring."""
+class Mage(Character):
+    ...
 
-    def first(self):
-        """Описывает метод first и демонстрирует перенос строки
-        документации.
-        """
-        ...
- 
 
-print('Без применения cleandoc:')
-print(Test.first.__doc__)
-print('С применением cleandoc:')
-print(inspect.cleandoc(Test.first.__doc__))
-# Выведите докстринг, используя метод cleandoc().
+class Healer(Character):
+    ...           
+
+
+def attack(char_name: str, char_class: str) -> str:
+    """Ввод персонажа и его уровень атаки."""
+    if char_class == 'warrior':
+        return (f'{char_name} нанёс урон противнику равный '
+                f'{5 + randint(3,5)}')
+    if char_class == 'mage':
+        return (f'{char_name} нанёс урон противнику равный '
+                f'{5 + randint(5,10)}')
+    if char_class == 'healer':
+        return (f'{char_name} нанёс урон противнику равный '
+                f'{5 + randint(-3,-1)}')
+    return (f'{char_class} не ненанес урон противнику')
+
+
+def defence(char_name: str, char_class: str) -> str:
+    """Ввод персонажа и его уровень защиты."""
+    if char_class == 'warrior':
+        return (f'{char_name} блокировал {10 + randint(5,10)} урона')
+    if char_class == 'mage':
+        return (f'{char_name} блокировал {10 + randint(-2,2)} урона')
+    if char_class == 'healer':
+        return (f'{char_name} блокировал {10 + randint(2,5)} урона')
+    return (f'{char_class} не блокировал урон')
+
+
+def special(char_name: str, char_class: str) -> str:
+    """Выбор специальных возможностей персонажа."""
+    if char_class == 'warrior':
+        return (f'{char_name} применил специальное умение'
+                f' «Выносливость {80 + 25}»')
+    if char_class == 'mage':
+        return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
+    if char_class == 'healer':
+        return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
+    return (f'{char_class} не применил специальное умение')
+
+
+def start_training(char_name: str, char_class: str) -> str:
+    """Выбор умений персонажа."""
+    if char_class == 'warrior':
+        print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
+    if char_class == 'mage':
+        print(f'{char_name}, ты Маг — превосходный укротитель стихий.')
+    if char_class == 'healer':
+        print(f'{char_name}, ты Лекарь — чародей, способный исцелять раны.')
+    print('Потренируйся управлять своими навыками.')
+    print('Введи одну из команд: attack — чтобы атаковать противника, '
+          'defence — чтобы блокировать атаку противника или '
+          'special— чтобы использовать свою суперсилу.')
+    print('Если не хочешь тренироваться, введи команду skip.')
+    cmd: str = None
+    while cmd != 'skip':
+        cmd = input('Введи команду: ')
+        if cmd == 'attack':
+            print(attack(char_name, char_class))
+        if cmd == 'defence':
+            print(defence(char_name, char_class))
+        if cmd == 'special':
+            print(special(char_name, char_class))
+    return 'Тренировка окончена.'
+
+
+def choice_char_class() -> str:
+    """Выбор имени персонажа и его описание."""
+    approve_choice: str = None
+    char_class: str = None
+    while approve_choice != 'y':
+        char_class = input('Введи название персонажа, '
+                           'за которого хочешь играть: '
+                           'Воитель — warrior, '
+                           'Маг — mage, '
+                           'Лекарь — healer: ')
+        if char_class == 'warrior':
+            print(
+                'Воитель — дерзкий воин ближнего боя.'
+                ' Сильный, выносливый и отважный.')
+        if char_class == 'mage':
+            print('Маг — находчивый воин дальнего боя. '
+                  'Обладает высоким интеллектом.')
+        if char_class == 'healer':
+            print('Лекарь — могущественный заклинатель. '
+                  'Черпает силы из природы, веры и духов.')
+        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
+                               'или любую другую кнопку, '
+                               'чтобы выбрать другого персонажа').lower()
+    return char_class
+
+
+if __name__ == '__main__':
+    # run_screensaver()
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class()
+
+    print(start_training(char_name, char_class))
